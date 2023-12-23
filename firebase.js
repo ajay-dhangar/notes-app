233715a38299b2e270bf6d548854925623b5bcec
Ajay-Dhangar <ajaydhangar49@gmail.com>
2023-12-23 13:42:53 +0530

    setup directories for GitHub Pages

--- firebase.js
+++ firebase.js
@@ -3,13 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getFirestore, collection} from 'firebase/firestore';
 
+// Create YOUR FIREBASE PROJECT and Replace to or copy paste "firebaseConfig"
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBzYbKPDsOjEjYQPsevvcOND5kYNHg84OQ",
-  authDomain: "notes-and-tenzies-d1c9d.firebaseapp.com",
-  projectId: "notes-and-tenzies-d1c9d",
-  storageBucket: "notes-and-tenzies-d1c9d.appspot.com",
-  messagingSenderId: "204770573975",
-  appId: "1:204770573975:web:0715e092266d3ffa78b3d2",
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN", 
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID",
   measurementId: "G-EKZR0PX1VN"
 };
 
